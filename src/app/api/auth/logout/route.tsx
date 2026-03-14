60e83f935dad43de690688eb8c54caf862a71ba5
saurabh-yadav548 <saurabhkumar1542007@gmail.com>
2026-03-15 01:49:36 +0530

    set up login/signup

--- src/app/api/auth/logout/route.tsx
+++ src/app/api/auth/logout/route.tsx
@@ -1,7 +1,25 @@
-const logout = () => {
-  return (
-    <div>  route</div>
-  )
-}
+import {NextResponse } from "next/server"
 
-export default logout
+
+export async function POST(){
+
+  try{
+    const response = NextResponse.json(
+    {message:"Loggedout sucessfully"},{status:200}
+  );
+
+  response.cookies.set("token" ,"" ,{
+    httpOnly :true,
+    expires:new Date(0),
+    path:'/'
+  });
+
+  return response;
+  }catch(error:unknown){
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+
+}
